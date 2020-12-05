09338cd147a27dabb4f556fe668a9dcabb9746ff
이준화 <62980975+wnsghk16@users.noreply.github.com>
2020-12-05 23:07:58 +0900

    스프링 웹 MVC 11 CORS

---
+++ src/test/java/me/junhwa/springcrosserver/SpringcorsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package me.junhwa.springcrosserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorsserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
